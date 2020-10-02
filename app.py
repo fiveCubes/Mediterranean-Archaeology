6da699f65f000d5cb91b9a00dc989beccdc1590b
vishnu <vishnuvijayakumar125@gmail.com>
2020-10-02 13:41:42 +1000

    details spell check

--- app.py
+++ app.py
@@ -7,6 +7,6 @@ app=Flask(__name__)
 def index():
     pdf_ex = pdf_extractor('./newRvp3.pdf')
     p_list= [p.get_attributes() for p in pdf_ex.get_paragraphs()]
-    return jsonify({"success": True , "Vase_detials":p_list})
+    return jsonify({"success": True , "Vase_details":p_list})
 
 
