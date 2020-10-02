c16c37d12c5abf2b67d19a8ad3b5277231505baf
vishnu <vishnuvijayakumar125@gmail.com>
2020-10-02 12:36:56 +1000

    deployment testing

---
+++ Paragraph.py
@@ -0,0 +1,158 @@
+import re
+class Paragraph:
+    previous=''
+    shape_pattern =f'.+?(?=\*\d)'
+    museum_pattern1=f'(\*\d.+?(?=\. Ht|ht))|(\d.+?(?=\. Ht|ht))'
+    museum_pattern2=f'(\*\d.+?(?=\. [A-Z]))|(\d.+?(?=\. [A-Z0-9]))'
+    dimension_pattern1='(Ht|ht).+?(?=[A-Z])' 
+    dimension_pattern2='\. \d.+?(?:x).+?(\.)'
+    plate_pattern1='(PLATE.+?(?=[A-Z]))|(PLATED.+?(?=[A-Z]+))'
+    extras_pattern1='((PP|PAdd|PPSupp).+?(?=\. \([a-z]\)))|((PP|PAdd|PPSupp).+?(?:fig\. [0-9]+))|((PP|PAdd|PPSupp).+?(?=\. [A-Z][A-Za-z]+))'
+    extras_pattern2='(PLATE.+?(?= \([a-z]\)))'
+    extras_pattern3='PLATE.+?(?=[A-Z])'
+
+    def __init__(self,text,start):
+        self.text = text
+        self.attributes = {
+            'id':start,
+            'shape':'' ,
+            'location':'',
+            'plate':'',
+            'dimension':'',
+            'extras':'',
+            'description':''
+
+        }
+       
+    
+    def find_match(self,pattern):
+        search = re.search(pattern,self.text)
+        if (search):
+            return search.group(),search.span()
+
+        return search
+
+
+    def get_shape(self):
+        shape = self.find_match(Paragraph.shape_pattern)
+        if(shape):
+            Paragraph.previous=shape
+            return shape
+        else:
+            return Paragraph.previous
+
+    def get_location(self):
+        location = self.find_match(Paragraph.museum_pattern1)
+        if(location):
+            return location
+        else:
+            location= self.find_match(Paragraph.museum_pattern2)
+            if(location):
+                return location
+        return None
+
+    def get_dimension(self):
+        dimension = self.find_match(Paragraph.dimension_pattern1)
+        if(dimension):
+            return dimension
+        else:
+            dimension = self.find_match(Paragraph.dimension_pattern2)
+            if(dimension):
+                return dimension
+        return None
+   
+    def get_plate(self):
+        plate = self.find_match(Paragraph.plate_pattern1)
+        if(plate):
+            return plate
+        return None
+
+    def get_extras(self):
+        extras = self.find_match(Paragraph.extras_pattern1)
+        if(extras):
+            return extras
+        else:
+            extras = self.find_match(Paragraph.extras_pattern2)
+            if(extras):
+                return extras
+            else:
+                extras = self.find_match(Paragraph.extras_pattern3)
+                if(extras):
+                    return extras
+        return None
+
+    def get_description(self):
+        extras = self.get_extras()
+        plate=self.get_plate()
+        dimension=self.get_dimension()
+
+        if(extras):
+            span= extras[1]
+            return self.text[span[1]:]
+        elif(plate):
+            span=plate[1]
+            return self.text[span[1]:]
+        elif(dimension):
+            span=dimension[1]
+            return self.text[span[1]:]
+
+    def __str__(self):
+        return str(self.get_shape()[0])
+
+  
+
+    def format_attributes(self):
+        location= self.get_location()
+        extras = self.get_extras()
+        plate =self.get_plate()
+        description = self.get_description()
+        shape = self.get_shape()
+        dimension = self.get_dimension()
+        if (location):
+            start=self.attributes['id']
+            location= location[0].replace(f'*{start}','').strip()
+            self.attributes['location']=location
+        if(extras):
+            # remove "PLATE" from extras
+           if('PLATE' in extras[0]):
+                extras = re.sub(r'PLATE.+?(?=[A-Z])', "", extras[0])
+                if('PLATE' in extras):
+                    self.attributes['extras']=None
+                else:
+                    self.attributes['extras']=extras
+           else:
+               self.attributes['extras']=extras[0]
+        if(plate):
+            #remove (a) from the plates
+             plate= re.sub(r'\([a-z]\)',"",plate[0])
+             self.attributes['plate']=plate.strip()
+        if(shape):
+            self.attributes['shape']=shape[0]
+        if(dimension):
+            self.attributes['dimension']=dimension[0]
+        if(description):
+            self.attributes['description']=description
+        
+        return self.attributes
+              
+        
+             
+
+
+    
+
+    def get_attributes(self):
+        formatted_attr = self.format_attributes()
+        return formatted_attr
+
+        
+
+if(__name__=='__main__'):
+
+    p = Paragraph('Neck-amphora *1 Geneva HR 29. Ht. 42-3. PLATE 15 Chamay, La Guerre de Troie, pp. 24Š5, ill. on p. 25; Genava 33, 1985, p. 185, fig. 1; Le Peinlre de Darius et son milieu ( 1986), pp. 264Š9, with colour ill. on p. 29; detail, Musees de Geneve 265, May 1986, p. 17. (a) Orestes and Pylades at the tomb of Agamemnon, on the base of which Electra is kneeling, a phiale in her 1. hand and her r. on top of a hydria; aboveŠbusts of two Furies, (b) nude youth with strigil and stick between draped youth (A 1) and draped woman, each with spray. Neck: (a) Siren, holding fillet and tambourine, (b) female head in profile to 1')
+    print(p.get_shape())
+    print(p.get_location())
+    print(p.get_dimension())
+    print(p.get_plate())
+    print(p.get_extras())
+    print(p.get_description())
---
+++ Pdf_extractor.py
@@ -0,0 +1,62 @@
+import PyPDF2
+import re
+from Paragraph import Paragraph
+class pdf_extractor:
+    def __init__(self,file_path):
+        self.file_path=file_path
+        self.whole_text=''
+        self.paragraphs_list=[]
+    
+
+    def load_text(self):
+        with open(self.file_path,'rb') as f:
+            pdf = PyPDF2.PdfFileReader(f,strict=False)
+            for i in range(25):
+                page = pdf.getPage(i)
+                text = page.extractText()
+                self.whole_text= self.whole_text + text
+            #corrections in the whole text
+            self.whole_text=self.whole_text.replace('Š','-').replace('THE RED-FIGURED VASES OF PAESTUM','').replace('I9','19')
+            
+            
+
+    def get_paragraph(self):
+            self.load_text()
+            for i in range(1,58):
+                start = int(i)
+                end = int(i)+1
+                pattern = f"(Fragments|Fragment \(.+\)| Lebes gamikos| Squat lekythos| [a-zA-Z]+-[a-zA-z]+| [a-zA-Z]+|) (\*{start} )(.+?)((PLATE|Plate).+?)(?=( \*{end} | {end} | The|6The | On | With))"
+                search= re.search(pattern,self.whole_text)
+                if(search==None):
+                    pass
+                    pattern2 = f'{start} .+?(?=(\*{end} | {end} | The | On | With))'
+                    search2= re.search(pattern2,self.whole_text)
+                    paragraph =search2.group()
+                    paragraph= paragraph[0:paragraph.rindex('.')].replace('[','(')
+                    para_object = Paragraph(paragraph,start)
+                    self.paragraphs_list.append(para_object)
+                else:
+                    paragraph=search.group().strip() 
+                    paragraph= paragraph[0:paragraph.rindex('.')].replace('[','(')
+                    para_object = Paragraph(paragraph,start)
+                    self.paragraphs_list.append(para_object)
+                    s, e = search.span()
+                    self.whole_text = self.whole_text[e-30:]
+            return self.paragraphs_list
+
+if(__name__=='__main__'):  
+    pdf_ex = pdf_extractor('./newRVp3.pdf')
+    p_list=pdf_ex.get_paragraph()
+    out = open('demo1.txt','a')
+    for i in p_list:
+        print(i)
+        x=i.get_attributes()
+        out.write(str(x))
+        out.write('\n_____________\n')
+        print('*************')
+    out.close()
+                    
+
+
+
+    
